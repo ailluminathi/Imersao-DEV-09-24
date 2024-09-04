ca50f5ac575c0314057b7bce7fee7f657b4e9d86
Náthali Santos <108452211+ailluminathi@users.noreply.github.com>
2024-09-04 14:16:26 -0300

    aula 3

---
+++ dados.js
@@ -0,0 +1,17 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que encantou o mundo com seu talento e carisma. Com apenas [idade da Rayssa], ela já conquistou diversas medalhas e se tornou um ícone da nova geração do skate.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris. Com sua técnica impecável e força, ela conquistou a medalha de ouro e se tornou um exemplo para jovens atletas.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma das maiores ginastas brasileiras de todos os tempos. Com sua força, agilidade e determinação, ela conquistou o coração dos brasileiros e se tornou um símbolo de superação e sucesso.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    }
+];
