af12dd392252fc72cde962c5ecffdec14c1fe8f5
Náthali Santos <108452211+ailluminathi@users.noreply.github.com>
2024-09-05 11:59:49 -0300

    aula 4

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log(dados[0].descricao);
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+  
+    // Inicializa uma string vazia para armazenar os resultados formatados
+    let resultados = "";
+  
+    // Itera sobre cada elemento (dado) do array 'dados'
+    for (let dado of dados) {
+      // Concatena HTML para criar um novo elemento de resultado
+      resultados += `
+        <div class="item-resultado">
+          <h2>
+            <a href="#" target="_blank">${dados[0].titulo}</a>
+          </h2>
+          <p class="descricao-meta">${dado.descricao}</p>
+          <a href="${dado.link}" target="_blank">Mais informações</a>
+        </div>
+      `;
+    }
+  
+    // Atribui a string com os resultados ao conteúdo HTML da seção
+    section.innerHTML = resultados;
+  }
